ab3f37b394637da5ac4038e32e53d0a21425ab92
Luan Flavio Barros Menezes <luanflavio.bm@gmail.com>
2023-10-13 14:11:55 -0300

    [BugFix] Grafico pareto consertado quando é quanti/quali

--- paretoExel.py
+++ paretoExel.py
@@ -102,13 +102,15 @@ def operacoesExel(origemArquivo, localDoArquivo, lista, tipo=1, custo_de_ocorren
         # Pegando as informações da coluna A
         tipo_de_falha = list()
         numero_de_ocorrencias = list()
+        totalCusto = list()
         tipo_de_falha = list(set(lista))
 
         # Ordenando em ordem descrecente as ocorrências!
         for i, ocorrencia in enumerate(tipo_de_falha):
             nOcorrencia = lista.count(ocorrencia)
-            numero_de_ocorrencias.append([ocorrencia, nOcorrencia, custo_de_ocorrencia[i]])
-        numero_de_ocorrencias = sorted(numero_de_ocorrencias, key=lambda x: x[1], reverse=True)
+            totalCusto.append(nOcorrencia * custo_de_ocorrencia[i])
+            numero_de_ocorrencias.append([ocorrencia, nOcorrencia, custo_de_ocorrencia[i], totalCusto[i]])
+        numero_de_ocorrencias = sorted(numero_de_ocorrencias, key=lambda x: x[3], reverse=True)
 
 
         # Títulos
@@ -131,6 +133,7 @@ def operacoesExel(origemArquivo, localDoArquivo, lista, tipo=1, custo_de_ocorren
             sheet[celula] = cell[0]
             sheet[f'D{i+1}'] = cell[1]
             sheet[f'E{i+1}'] = cell[2]
+            sheet[f'F{i+1}'] = cell[3]
             soma += cell[1]
             if cell == numero_de_ocorrencias[-1]:
                 sheet[f'C{len(numero_de_ocorrencias)+2}'] = "Total"
@@ -139,13 +142,13 @@ def operacoesExel(origemArquivo, localDoArquivo, lista, tipo=1, custo_de_ocorren
                 sheet[f'E{len(numero_de_ocorrencias)+2}'] = '-'
 
                 # Fazendo o Total(R$)
-                for i in range(1, len(numero_de_ocorrencias)+1):
-                    sheet[f'F{i+1}'] = f"=E{i+1}*D{i+1}"
+                # for i in range(1, len(numero_de_ocorrencias)+1):
+                #     sheet[f'F{i+1}'] = f"=E{i+1}*D{i+1}"
                 sheet[f'F{len(numero_de_ocorrencias)+2}'] = f"=SUM(F2:F{len(numero_de_ocorrencias)+1})"
 
                 # Fazendo Fr (%)
                 for i in range(1, len(numero_de_ocorrencias)+1):
-                    sheet[f'G{i+1}'] = f"=D{i+1}/ D{len(numero_de_ocorrencias)+2}"
+                    sheet[f'G{i+1}'] = f"=F{i+1}/ F{len(numero_de_ocorrencias)+2}"
                 sheet[f'G{len(numero_de_ocorrencias)+2}'] = f'=SUM(G2:G{len(numero_de_ocorrencias)+1})'
 
                 # Fazendo Fr Acumulada (%)
@@ -178,8 +181,9 @@ def aplicacaoGraficoPareto(localDoArquivo, gerarExel=False, tipo=1):
     def pegarExel():
         tipoDefeito = list()
         numeroOcorrencias = list()
-        frequecia = list()
-        frequeciaAcumulada = list()
+        frequencia = list()
+        frequenciaAcumulada = list()
+        totalCusto = list()
 
         ultimaLinha = 0
         numeroCelula = 2
@@ -202,37 +206,56 @@ def aplicacaoGraficoPareto(localDoArquivo, gerarExel=False, tipo=1):
                 celula = sheet[f'E{fr}'].value
             else:
                 celula = sheet[f'G{fr}'].value
-            frequecia.append(celula * 100)
+            frequencia.append(celula * 100)
 
         for fracum in range(2, len(tipoDefeito)+2):
             if tipo == 1:
                 celula = sheet[f'F{fracum}'].value
             else:
                 celula = sheet[f'H{fracum}'].value
-            frequeciaAcumulada.append(celula * 100)
+            frequenciaAcumulada.append(celula * 100)
 
+        if tipo != 1:
+            for fracum in range(2, len(tipoDefeito)+2):
+                celula = sheet[f'F{fracum}'].value
+                totalCusto.append(celula)
 
-        return tipoDefeito, numeroOcorrencias, frequecia, frequeciaAcumulada, ultimaLinha
+        if tipo == 1:
+            return tipoDefeito, numeroOcorrencias, frequencia, frequenciaAcumulada, ultimaLinha
+        else: 
+            return tipoDefeito, numeroOcorrencias, totalCusto, frequencia, frequenciaAcumulada, ultimaLinha
 
-
-    tipo_de_falha, numeroOcorrencias, frequencia, frequeciaAcumulada, ultimaLinha = pegarExel()
-
-    plt.rcParams.update({'font.size': 14})
-    df = pd.DataFrame({'tipo defeito' : tipo_de_falha,
+    if tipo == 1:
+        tipo_de_falha, numeroOcorrencias, frequencia, frequeciaAcumulada, ultimaLinha = pegarExel()
+        df = pd.DataFrame({'tipo defeito' : tipo_de_falha,
                     'Nº Ocor' : numeroOcorrencias, 
                     'freq (%)': frequencia,
                     'cum (%)' : frequeciaAcumulada})
+    else:
+        tipo_de_falha, numeroOcorrencias, totalCusto, frequencia, frequeciaAcumulada, ultimaLinha = pegarExel()
+        df = pd.DataFrame({'tipo defeito' : tipo_de_falha,
+                    'Nº Ocor' : numeroOcorrencias,
+                    'Total Custo' : totalCusto,
+                    'freq (%)': frequencia,
+                    'cum (%)' : frequeciaAcumulada})
 
+    plt.rcParams.update({'font.size': 14})
+    
     fig,ax1 = plt.subplots(figsize = (12,6))
 
     ax1.set_title('Pareto')
 
     color1 = 'tomato'
     #ax1.set_xlabel('X')
-    ax1.set_ylabel('Nº Ocorrências',color = color1)
 
-    bars = ax1.bar(df['tipo defeito'], df['Nº Ocor'],color = color1,edgecolor = 'orange',linewidth = 2,\
-                    hatch = '*')
+    if tipo == 1:
+        ax1.set_ylabel('Nº Ocorrências',color = color1)
+        bars = ax1.bar(df['tipo defeito'], df['Nº Ocor'],color = color1,edgecolor = 'orange',linewidth = 2,\
+                        hatch = '*')
+    else:
+        ax1.set_ylabel('Impacto de Custo',color = color1)
+        bars = ax1.bar(df['tipo defeito'], df['Total Custo'],color = color1,edgecolor = 'orange',linewidth = 2,\
+                        hatch = '*')
     #ax1.set_ylim([-10,10])
     ax1.tick_params(axis = 'y',labelcolor = color1)
 
@@ -317,7 +340,7 @@ def lerarquivo(localDoArquivo, tipo):
                 d = sheet[f"D{linha}"].value
                 e = sheet[f"E{linha}"].value
                 f = round(float(sheet[f"F{linha}"].value), 2)
-                g = soma_fr
+                g = round(soma_fr, 1)
                 h = sheet[f"H{linha}"].value
                 linhas.append([c, d, e, f, g, h])
 
